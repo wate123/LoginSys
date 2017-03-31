57a0efc1c4a8fd9cecef2173d72e5d94d6c6951d
Jun Lin <wate11111@gmail.com>
2017-03-31 18:37:24 -0500

    Most recent

--- src/LoginSysController.java
+++ src/LoginSysController.java
@@ -5,60 +5,34 @@ import java.sql.*;
 /**
  * Created by Jun Lin on 3/30/2017.
  */
-public class LoginSysController{
-    private Connection conn = null;
-    private PreparedStatement pt = null;
-    private ResultSet rs = null;
+public class LoginSysController {
     private LoginSysView theView;
     private LoginSysModel theModel;
 
 
-    public void LoginSysView(LoginSysView theView, LoginSysModel theModel) {
+    public LoginSysController (LoginSysView theView, LoginSysModel theModel) {
         this.theView = theView;
         this.theModel = theModel;
-        //this.theView.addSubmitListener(SubmitActionPerformed());
-    }
-    private  void checkNameActionPerformed(ActionEvent e){
-        conn = LoginSysModel.ConnectDB();
-        String sql = "username=?";
-        try{
-            pt = conn.prepareStatement(sql);
-            pt.setString(1, theView.getInName().getText());
-            rs = pt.executeQuery();
-            if(rs.next()){
-                JOptionPane.showMessageDialog(null,"Vaild user");}
-            else {
-                JOptionPane.showMessageDialog(null,"Invaid Username");
-            }
-            }catch (Exception d){
-            JOptionPane.showMessageDialog(null,d);
-        }
-    }
-    private void SubmitActionPerformed(ActionEvent evt){
-        conn = LoginSysModel.ConnectDB();
-        String sql = "Select * from users_table Where username=? password=?";
-        try{
-            pt = conn.prepareStatement(sql);
-            pt.setString(1, theView.getInName().getText());
-            pt.setString(2,theView.getInPasswd().getText());
-            rs = pt.executeQuery();
+//        this.theView.addSubmitListener(new clickListener());
+//        this.theView.addCheckListener(new clickListener());
 
-            if(rs.next()){
-                JOptionPane.showMessageDialog(null,"Welcome user");
-                LoginSysView w = new LoginSysView();
-                w.welcome().setVisible(true);
-            }
-            else{
-                JOptionPane.showMessageDialog(null,"Invaild username or password","Access Denied", JOptionPane.ERROR_MESSAGE);
-            }
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Something is wrong with Mysql");
+    }
+
+    class clickListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            theModel.checkName(e);
         }
     }
+
+
     public static void main(String[] args){
         LoginSysView theView = new LoginSysView();
         LoginSysModel theModel = new LoginSysModel();
+        LoginSysController theController = new LoginSysController(theView,theModel);
         theView.LoginSysView();
+        theModel.checkUnamePasswd();
         theView.setVisible(true);
     }
+
 }
--- src/LoginSysView.java
+++ src/LoginSysView.java
@@ -5,14 +5,11 @@ import java.awt.event.ActionListener;
  * Created by jun on 3/29/17.
  */
 public class LoginSysView extends JFrame {
-    private JLabel title;
-    private JLabel userLab = new JLabel();
     private JLabel passwdLab = new JLabel();
     //Create a Text Field to input username and limit the username to 10 Digits.
     private JTextField inName = new JTextField(10);
-    //Create a Text Field to input password and limit the password to 10 Digits.
-    private JPasswordField inPasswd = new JPasswordField(10);
-    //Create a check button
+    //Create a Text Field to input password and limit the password to 30 Digits.
+    private JPasswordField inPasswd = new JPasswordField(30);
     private JButton checkButton;
     //Create a submit button
     private JButton submitButton;
@@ -27,38 +24,60 @@ public class LoginSysView extends JFrame {
     }
 
     private void Components(){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        title = new JLabel("Welcome to My System!");
-        userLab = new JLabel("Username");
+
+        JLabel title = new JLabel("Welcome to My System!", SwingConstants.CENTER);
+        JLabel userLab = new JLabel("Username", JLabel.RIGHT);
         checkButton = new JButton("Check");
         submitButton = new JButton("Submit");
-        setForeground(Color.blue);
-        setSize(400,100);
+        //Multiline Tips for password criteria
+        inPasswd.setToolTipText("<html>"
+                +"1. Must have at least 8 characters"
+                +"<br>"
+                +"2. First character must be letter"
+                +"<br>"
+                +"3. Must have at least one letter and one number"
+                +"<br>"
+                +"4. Only letter or digit." +
+                "</html>");
+        setTitle("Log into Jun's DataBase");
+        this.setSize(400,300);
         setVisible(true);
-        loginPan.setSize(600,600);
+
+        userLab.setVerticalTextPosition(JLabel.CENTER);
+        //Focus on the Username input field
+        inName.requestFocus();
         add(loginPan);
         loginPan.add(title);
         loginPan.add(userLab);
         loginPan.add(inName);
         loginPan.add(checkButton);
         loginPan.setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public JFrame welcome(){
         welText.setText("Welcome "+getInName());
         welcome.add(welText);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         return welcome;
     }
 
-    public void addSubmitListener(ActionListener listenSubmit){
-        submitButton.addActionListener(listenSubmit);
+//    public void addCheckListener(ActionListener listenCheck){
+//        checkButton.addActionListener(listenCheck);
+//    }
+//
+//    public void addSubmitListener(ActionListener listenSubmit){
+//        submitButton.addActionListener(listenSubmit);
+//    }
+    public void displayUsernameVaild(){
+        JOptionPane.showMessageDialog(this,"Vaild Username");
     }
-    public void dispayPassMessage(){
-         JOptionPane.showMessageDialog(null,"Welcome "+getInName());
+    public void displayLoginMessage(){
+         JOptionPane.showMessageDialog(this,"Welcome "+getInName());
     }
 
-    public void dispayErrorMessage(String errorMessage){
-        JOptionPane.showMessageDialog(this,errorMessage);
+    public void displayErrorMessage(String errorMessage, String til){
+        JOptionPane.showMessageDialog(this,errorMessage,til,JOptionPane.ERROR_MESSAGE);
     }
 
     public JTextField getInName() {
